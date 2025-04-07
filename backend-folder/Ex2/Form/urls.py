88334d28507036e5b1c0979ab1b5696ece2be3f2
mohammad amin <mat.chenari.computer@gmail.com>
2025-04-07 10:41:12 +0330

    add gitignore and simple views

--- backend-folder/Ex2/Form/urls.py
+++ backend-folder/Ex2/Form/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
+from .views import SendForm, GetForm
 
 urlpatterns = [
-    path(r'send'),
-    path(r'get')
+    path(r'send', SendForm.as_view(), name='send-form'),
+    path(r'get', GetForm.as_view(), name='get-form')
 ]
--- backend-folder/Ex2/Form/views.py
+++ backend-folder/Ex2/Form/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework.generics import CreateAPIView, ListAPIView
 # Create your views here.
+
+class SendForm(CreateAPIView):
+    pass
+
+class GetForm(ListAPIView):
+    pass
