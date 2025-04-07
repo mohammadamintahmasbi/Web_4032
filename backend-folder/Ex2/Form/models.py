f479dcd2506335feddad4c6a84b26b5561e68cb2
mohammad amin <mat.chenari.computer@gmail.com>
2025-04-07 10:37:25 +0330

    create form models

--- backend-folder/Ex2/Form/models.py
+++ backend-folder/Ex2/Form/models.py
@@ -1,11 +1,22 @@
 from django.db import models
-
+from django.utils.translation import gettext as _
 # Create your models here.
 
 class Form(models.Model):
     class Education(models.TextChoices):
-        pass
+        BACHELOR = 'Bachelor', _('Bachelor')
+        MASTER = 'Master', _('Master')
+        PH_D = 'PH.D', _('PH.D')
+        
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField()
+    education_degree = models.CharField(
+        choices=Education,
+        help_text=_('Please select one of the options'),
+        blank=True,
+        null=True,
+        max_length=8
+    )
+
     
